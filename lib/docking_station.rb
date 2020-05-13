4d00d98d90574a35fd0916e560c6d9d55cc31b76
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-05-13 16:10:47 +0100

    Raise exception added but not passing rspec tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,11 +5,14 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
+    fail "No bikes available" if empty?
     @bike = Bike.new
   end
 
-  def dock
 
+
+  def dock(bike)
+    @bike 
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,23 +2,24 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-describe "#release_bike" do
 
-  it "should respond to release_bike" do
-    expect(subject).to respond_to(:release_bike)
+  describe "#release_bike" do
+    it "should respond to release_bike" do
+      expect(subject).to respond_to(:release_bike)
+    end
+
+    it "should release working bikes" do
+      expect(subject.release_bike.working?).to eq true
+    end
+
+    it "should not release working bikes" do
+      expect { subject.release_bike }.to raise_error("No bikes available")
+    end
   end
-
-  it "should release working bikes" do
-   expect(subject.release_bike.working?).to eq true
-   # bike = subject.release_bike
-   # expect(bike).to be_working
+  
+  describe "#dock" do
+    it "allows a bike to be docked " do
+      expect(subject).to respond_to(:dock)
+    end
   end
-end
-
-describe "#dock" do
-
-  it "allows a bike to be docked " do
-    expect(subject).to respond_to(:dock)
-  end
-end
 end
