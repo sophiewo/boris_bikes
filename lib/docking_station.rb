5ef82aa0611beb6a7695a11d71c446527ba7119c
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-05-15 12:35:05 +0100

    SRP refactor of capacity methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,16 +9,26 @@ attr_reader :bike
   end
 
   def release_bike
-    fail "No bikes available" unless @bike_store.count >= 1
+    fail "No bikes available" if empty?
     @bike_store.pop
   end
 
-
-
   def dock(bike)
-    fail "dock full" if @bike_store.count >= 20
+    fail "dock full" if full?
     @bike_store << bike
-  
   end
 
+  private
+
+  def full?
+    if @bike_store.count >= 20
+      true
+    end
+  end
+
+  def empty?
+    if @bike_store.count == 0
+      true
+    end
+  end
 end
